c978c6447b4f38c6c9542ef7be13f33ca1fb03f2
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-01-29 14:13:57 +0530

    feat: Implement user registration and login views
    Added views for user registration and login functionality:
    - Created 'register' view to handle user registration form submissions.
    - Implemented username, email, and password validation during registration.
    - Utilized Django's authentication system to handle user login.
    - Enhanced code readability and structure.

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 def index(request):
-  return render(request, 'index.html')
+  return render(request, 'home.html')
+
+def register(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    first_name = request.POST['first_name']
+    email = request.POST['email']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+
+    if password1 == password2:
+      if User.objects.filter(username=username).exists():
+          print('Username already taken')
+      elif User.objects.filter(email=email).exists():
+          print('Email already registered')
+      else:
+          user = User.objects.create_user(
+              username=username,
+              first_name=first_name,
+              email=email,
+              password=password1
+          )
+          user.save()
+          print('User registration successful')
+          return redirect('index')
+    else:
+          print('Passwords do not match')
+
+  return render(request, 'registerations.html')
+
+def login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        auth_login(request, user)  # Rename login to auth_login
+        print('Login successful')
+        return redirect('index')
+    else:
+        print('Invalid login credentials')
+        return redirect('login')
+
+  return render(request, 'registerations.html')
